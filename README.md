0af8789bd83e55a5ad0bd6dabf0c1daf947b1422
eduCarv <edu.ascarvalho@gmail.com>
2021-01-25 22:00:03 -0300

    Adiciona Readme - Conclusao tutorial Guilherme Rodz

---
+++ README.md
@@ -0,0 +1,12 @@
+# Clone da interface do app da Twitch.
+
+## * Importante *
+ - Este é um projeto iniciado a partir do tutorial do Guilherme Rodz da Rocketseat
+ que pode ser visto [aqui](https://www.youtube.com/watch?v=bJVp_vlvMwQ&list=PL85ITvJ7FLohTZv9cC5-PrZ39Q3cugWqp&index=7)
+
+ - Basicamente o tutorial ensina alguns conceitos muito bons sobre o desenvolvimento em
+React Native enquanto recriamos a tela de Following (Seguindo) do App.
+
+## * Objetivos *
+- Recriar a interface das telas principais deste ano (2021) inclusive os efeitos para fixar os conhecimentos
+em React Native.
---
+++ src/components/CategoryList/data.ts
@@ -0,0 +1,15 @@
+import lol from '../../images/category_lol.jpg';
+import valorant from '../../images/category_valorant.jpg';
+import csgo from '../../images/category_csgo.jpg';
+import fortnite from '../../images/category_fortnite.jpg';
+import fallguys from '../../images/category_fallguys.jpg';
+import apex from '../../images/category_apex.jpg';
+
+export default [
+  { name: 'League of Legends', source: lol, viewers: '51.9k' },
+  { name: 'VALORANT', source: valorant, viewers: '22.1k' },
+  { name: 'Counter-Strike: Global Offensive', source: csgo, viewers: '62.6k' },
+  { name: 'Fortnite', source: fortnite, viewers: '14.1k' },
+  { name: 'Fall Guys', source: fallguys, viewers: '11.1k' },
+  { name: 'Apex Legends', source: apex, viewers: '1.2k' },
+];
